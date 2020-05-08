606e3232d69816060a4d44b517bdb53835de5a78
Sajid <sajidalam786@gmail.com>
2020-05-08 16:38:00 +0100

    Update

--- LaunchServer.bat
+++ LaunchServer.bat
@@ -1,2 +1,2 @@
-java -Xms8G -Xmx8G -XX:PermSize=256m -Dfml.read.Timeout=560 -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+java -Xms8G -Xmx8G -Dfml.read.Timeout=560 -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
 pause
